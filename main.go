7804919b77181cf9a1006f6fd95dfad6061f92d5
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-07-09 18:42:02 +0530

    Created our first web server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
+}
 
 func main() {
-	fmt.Println("Hello World !")
+	// maps the root to a method
+	http.HandleFunc("/", handlerFunc)
+	// starts the server
+	http.ListenAndServe(":3000", nil)
 }
