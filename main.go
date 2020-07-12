4a606fe8527e2c375a9f9add472a22c13cf5f706
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-07-12 19:38:52 +0530

    Added a contact page

--- main.go
+++ main.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+/*	Notes
+ *
+ * printf(...) is equivalent to fprintf(stdout,...).
+ * fprintf is used to output to stream.
+ * sprintf(buffer,...) is used to format a string to a buffer.
+ *
+ */
+
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	// Set Content type to indicate the browser how to render page,
 	// if text/plain is used, it won't parse html tags, and out page as it is.
@@ -13,7 +21,20 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 
 	// print to fresh console
 	fmt.Println("Someone visited our website")
-	fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		// we can also use `` around string, to spread it into multiple lines.
+		fmt.Fprint(w, "To get in touch, please send us a mail to <a href=\"mailto:shutterbox@gmail.com\"> shutterbox@gmailcom </a>.")
+		fmt.Fprint(w, "<br><br>")
+		fmt.Fprint(w, "Thanks For Visiting!!!")
+	} else {
+		fmt.Fprint(w, "<h1> 404, Page Not found !!!")
+	}
+
+	// format the output
+	//fmt.Fprintf(w, "<p>This is your current path: %v", r.URL.Path)
 }
 
 func main() {
