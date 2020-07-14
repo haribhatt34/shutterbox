6efe02bfa7de23dd5a44ac4a79c5ae6959f67cb9
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-07-14 20:10:32 +0530

    Revert "added additionNotes.go"
    This reverts commit 614e032503ab6b3434e82873eb31c003f33a7cd1.

--- main.go
+++ main.go
@@ -3,24 +3,51 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1> Welcome to my awesome site </h1>")
-}
+/*	Notes
+ *
+ * printf(...) is equivalent to fprintf(stdout,...).
+ * fprintf is used to output to stream.
+ * sprintf(buffer,...) is used to format a string to a buffer.
+ *
+ */
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	// Set Content type to indicate the browser how to render page,
+	// if text/plain is used, it won't parse html tags, and out page as it is.
+	// w.Header().Set("Content-Type", "text/plain")
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "To get in touch, please send an email to <a href=\"mailto:shutterbox@gmail.com\">shutterbox@gmail.com</a>.")
+
+	// print to fresh console
+	fmt.Println("Someone visited our website")
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
+	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/" {
+		// we can also use `` around string, to spread it into multiple lines.
+		fmt.Fprint(w, "To get in touch, please send us a mail to <a href=\"mailto:shutterbox@gmail.com\"> shutterbox@gmailcom </a>.")
+		fmt.Fprint(w, "<br><br>")
+		fmt.Fprint(w, "Thanks For Visiting!!!")
+	} else {
+		// If WriteHeader is not called explicitly, the first call to Write
+		// will trigger an implicit WriteHeader(http.StatusOK).
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1> 404, Page Not found !!!")
+	}
+
+	// format the output
+	//fmt.Fprintf(w, "<p>This is your current path: %v", r.URL.Path)
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/contact/", contact)
-	http.ListenAndServe(":3000", r)
+	// A small Caveat, HandleFunc always matches the largest path,
+	// irrespective of the order, i.e. which mapping if defined early.
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", mux)
+	// maps the root to a method
+	//http.HandleFunc("/", handlerFunc)
+	// starts the server
+	//http.ListenAndServe(":3000", nil)
 }
