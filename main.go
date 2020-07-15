9d89dfa28211c054421f9b862500bc83b79d4fe8
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-07-15 22:18:45 +0530

    Added an FAQ Page

--- main.go
+++ main.go
@@ -17,10 +17,16 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "To get in touch, please send an email to <a href=\"mailto:shutterbox@gmail.com\">shutterbox@gmail.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> Welcome to FAQ Page")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/contact/", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
