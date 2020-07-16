34e6268163c41f5d701140f6c8b7df3dc7a5aec3
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-07-16 22:38:41 +0530

    Added a custom 404 page

--- main.go
+++ main.go
@@ -22,8 +22,15 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1> Welcome to FAQ Page")
 }
 
+func notfound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h2>Error 404, Page Not Found !!!</h2>")
+}
+
 func main() {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notfound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/contact/", contact)
