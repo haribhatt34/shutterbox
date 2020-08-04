ad320f220049c92e0add97f52de26950caa4fc4b
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-08-04 23:05:13 +0530

    Cleaned up our view.go code a bit

--- views/view.go
+++ views/view.go
@@ -1,17 +1,23 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var {
+	LayoutDir string = "views/layouts"
+	TemplateExt string = ".gohtml"
+}
 
 // NewView groups templates together
 // it takes variadic parameters
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/navbar.gohtml",
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/footer.gohtml",
-	)
+	// layoutfiles() will return a slice, but since
+	// append take variadic parameters, ... will
+	// unpack the slice and convert it strings.
+	files = append(files, layoutFiles()...)
 
-	//again, below slices withing files are unpacked
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -27,4 +33,14 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+// layoutFiles returns a slice of string reprenting
+// the layout files used in our application.
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
